4f10df1f61a0621cc30c966bdfc203013b77e182
Daniel Franks <danielkfranks@gmail.com>
2026-03-06 10:27:24 -0800

    servo code didn't get added in last commit xd

---
+++ DTC_Servos.h
@@ -0,0 +1,33 @@
+#include <Servo.h>
+
+int ResetRow(Servo[6] servoArray, int rowNumber) {
+	int upPosition = 35, downPosition = 180; // TODO tune these PLEASE
+	int idx1 = 0, idx2 = 0;
+
+	switch(rowNumber) {
+		case 1:
+			idx1 = 0;
+			idx2 = 1;
+			break;
+		case 2:
+			idx1 = 2;
+			idx2 = 3;
+			break;
+		case 3:
+			idx1 = 4;
+			idx2 = 5;
+			break;
+		default:
+			return -1;
+			break;
+	}
+
+	servoArray[idx1].write(upPosition);
+	servoArray[idx2].write(upPosition);
+	delay(15);
+	servoArray[idx1].write(downPosition);
+	servoArray[idx2].write(downPosition);
+
+	return 0;
+}
+	
